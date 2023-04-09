684550bb0a771815fc5f040d0a71caeb63a9a498
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-09 22:21:23 +0300

    edit: app file

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
+import {useAppDispatch} from './src/hooks/useAppDispatch';
+import {useAppSelector} from './src/hooks/useAppSelector';
+import {selectInitializeStatus} from './src/redux/initialize/initializeSelectors';
+import {initializeDataAsync} from './src/redux/initialize/asyncActions/initializeDataAsync';
+import {ReducerStatus} from './src/enums/ReducerStatus';
+import LoadingScreen from './src/screens/LoadingScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
-  return <HomeScreen />;
+  const dispatch = useAppDispatch();
+
+  const initializeStatus = useAppSelector(selectInitializeStatus);
+
+  useEffect(() => {
+    dispatch(initializeDataAsync());
+  }, [dispatch]);
+
+  return (
+    <>
+      {initializeStatus === ReducerStatus.loading && <LoadingScreen />}
+      {initializeStatus === ReducerStatus.finish && <HomeScreen />}
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/screens/LoadingScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, ActivityIndicator, Text} from 'react-native';
+
+import SafeView from '../components/SafeView';
+
+const LoadingScreen = () => {
+  return (
+    <SafeView contentContainerStyle={styles.container}>
+      <ActivityIndicator />
+
+      <Text style={styles.text}>Loading...</Text>
+    </SafeView>
+  );
+};
+
+export default LoadingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    marginTop: 10,
+  },
+});
