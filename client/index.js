d72fd438923b4f9151b08ce16237cc0e6c9cf230
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-09 21:54:30 +0300

    edit: init redux

--- client/index.js
+++ client/index.js
@@ -1,10 +1,16 @@
 import {AppRegistry} from 'react-native';
+import {Provider as StoreProvider} from 'react-redux';
 
 import App from './App';
 import {name as appName} from './app.json';
+import {store} from './src/redux/store';
 
 const RootApp = () => {
-  return <App />;
+  return (
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
+  );
 };
 
 AppRegistry.registerComponent(appName, () => RootApp);
