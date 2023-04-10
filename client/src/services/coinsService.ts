558127b14a2c7c7887e615de985621602276c436
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-10 11:07:48 +0300

    edit: coins service

--- client/src/services/coinsService.ts
+++ client/src/services/coinsService.ts
@@ -4,7 +4,7 @@ import {CoinJSON} from '../interfaces/CoinJSON';
 
 const getCoins = async () => {
   const {data} = await coinGeckoClient.get<CoinJSON[]>(
-    `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=true&price_change_percentage=7d&locale=en`,
+    `/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=7d&locale=en`,
   );
   return data.map(dataJSON => new Coin(dataJSON));
 };
