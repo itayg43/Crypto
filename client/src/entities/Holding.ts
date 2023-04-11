591b9b93bae27af403b10439292ecff7d688ff32
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-11 18:38:40 +0300

    edit: holding

--- client/src/entities/Holding.ts
+++ client/src/entities/Holding.ts
@@ -2,11 +2,29 @@ import {Coin} from './Coin';
 import {CoinJSON} from '../interfaces/CoinJSON';
 
 export class Holding extends Coin {
+  price7DaysAgo: number;
   quantity: number;
+  valueChangeIn7Days: number;
+  valueSparklineIn7Days: number[];
 
   constructor(coinJSON: CoinJSON, quantity: number) {
     super(coinJSON);
+    this.price7DaysAgo = this._calculatePrice7DaysAgo();
     this.quantity = quantity;
+    this.valueChangeIn7Days = this._calculateValueChangeIn7Days();
+    this.valueSparklineIn7Days = this._calculateValueSparklineIn7Days();
+  }
+
+  private _calculatePrice7DaysAgo() {
+    return this.currentPrice / (1 + this.priceChangePercentage7Days * 0.01);
+  }
+
+  private _calculateValueChangeIn7Days() {
+    return (this.currentPrice - this.price7DaysAgo) * this.quantity;
+  }
+
+  private _calculateValueSparklineIn7Days() {
+    return this.sparkline7Days.map(v => v * this.quantity);
   }
 
   getValue() {
