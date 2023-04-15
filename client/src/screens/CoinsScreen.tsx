093d6e25566573ec289bf3f8272913a597755405
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-15 18:59:16 +0300

    edit: add bottom sheet modal

--- client/src/screens/CoinsScreen.tsx
+++ client/src/screens/CoinsScreen.tsx
@@ -1,6 +1,14 @@
-import React, {useCallback, useEffect, useLayoutEffect, useState} from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+  useRef,
+  useMemo,
+} from 'react';
 import {StyleSheet} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 
 import {CoinsScreenNavigationProp} from '../navigation/CoinsStackNavigator';
 import {useAppDispatch} from '../hooks/useAppDispatch';
@@ -11,6 +19,7 @@ import {Coin} from '../entities/Coin';
 import useDebounce from '../hooks/useDebounce';
 import SafeView from '../components/SafeView';
 import CoinList from '../components/CoinList';
+import LineChart from '../components/LineChart';
 
 const CoinsScreen = () => {
   const dispatch = useAppDispatch();
@@ -22,13 +31,25 @@ const CoinsScreen = () => {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const debouncedSearchQuery = useDebounce(searchQuery);
 
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const bottomSheetModalSnapPoints = useMemo(() => ['50%'], []);
+
+  const handlePresetBottomSheetModal = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, [bottomSheetModalRef]);
+
+  const handleDismissBottomSheetModal = useCallback(() => {
+    setSelectedCoin(null);
+  }, []);
+
   const handleCoinSelection = useCallback(
     (coin: Coin) => {
       setSelectedCoin(currentCoin =>
         currentCoin?.id !== coin.id ? coin : currentCoin,
       );
+      handlePresetBottomSheetModal();
     },
-    [setSelectedCoin],
+    [setSelectedCoin, handlePresetBottomSheetModal],
   );
 
   const handleUpdateSearchQuery = useCallback(
@@ -52,15 +73,37 @@ const CoinsScreen = () => {
   }, [navigation, setSearchQuery]);
 
   return (
-    <SafeView>
-      {filteredCoins.length > 0 && (
-        <CoinList
-          containerStyle={styles.coinList}
-          data={filteredCoins}
-          onSelectItem={handleCoinSelection}
-        />
-      )}
-    </SafeView>
+    <>
+      <SafeView>
+        {filteredCoins.length > 0 && (
+          <CoinList
+            containerStyle={styles.coinList}
+            data={filteredCoins}
+            onSelectItem={handleCoinSelection}
+          />
+        )}
+      </SafeView>
+
+      <BottomSheetModalProvider>
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          snapPoints={bottomSheetModalSnapPoints}
+          index={0}
+          onDismiss={handleDismissBottomSheetModal}
+          style={styles.bottomSheetModal}
+          backgroundStyle={styles.bottomSheetModalBackground}
+          handleIndicatorStyle={styles.bottomSheetModalHandleIndicator}>
+          {selectedCoin && (
+            <LineChart
+              containerStyle={styles.lineChart}
+              data={selectedCoin.priceSparklineIn7Days}
+              dataRange={selectedCoin.priceRangeIn7Days}
+              dataChangePercentage={selectedCoin.priceChangePercentageIn7Days}
+            />
+          )}
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
+    </>
   );
 };
 
@@ -69,5 +112,25 @@ export default CoinsScreen;
 const styles = StyleSheet.create({
   coinList: {
     marginHorizontal: 10,
+  },
+
+  bottomSheetModal: {
+    shadowColor: 'gray',
+    shadowOffset: {
+      width: 0,
+      height: -5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  bottomSheetModalBackground: {
+    backgroundColor: 'black',
+  },
+  bottomSheetModalHandleIndicator: {
+    backgroundColor: 'gray',
+  },
+
+  lineChart: {
+    marginTop: 20,
   },
 });
