ed518a081b01c748a5e9f213fd732349a27986ff
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-04-19 00:09:27 +0300

    edit: execute market action

--- client/src/redux/holdings/asyncActions/executeMarketActionAsync.ts
+++ client/src/redux/holdings/asyncActions/executeMarketActionAsync.ts
@@ -5,22 +5,22 @@ import {updateHoldingAsync} from './updateHoldingAsync';
 import {deleteHoldingAsync} from './deleteHoldingAsync';
 
 export const executeMarketActionAsync =
-  (marketAction: MarketAction, id: string, quantity: number) =>
+  (action: MarketAction, id: string, quantity: number) =>
   async (dispatch: AppDispatch, getState: () => RootState) => {
     const holding = getState().holdings.entities[id];
     if (!holding) {
       const coin = getState().coins.entities[id];
       return dispatch(addHoldingAsync(coin, quantity));
     }
-    switch (marketAction) {
+    switch (action) {
       case MarketAction.buy: {
-        return dispatch(updateHoldingAsync(marketAction, id, quantity));
+        return dispatch(updateHoldingAsync(action, id, quantity));
       }
 
       case MarketAction.sell: {
         return holding.quantity === quantity
           ? dispatch(deleteHoldingAsync(id))
-          : dispatch(updateHoldingAsync(marketAction, id, quantity));
+          : dispatch(updateHoldingAsync(action, id, quantity));
       }
     }
   };
