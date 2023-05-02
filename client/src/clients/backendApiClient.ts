d286459567ed59f7292817d0bc376b3da9a1d837
itayg43 <93944494+itayg43@users.noreply.github.com>
2023-05-02 04:48:47 +0300

    edit: backend api client base url

--- client/src/clients/backendApiClient.ts
+++ client/src/clients/backendApiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const backendApiClient = axios.create({
-  baseURL: 'localhost:5000/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 export default backendApiClient;
